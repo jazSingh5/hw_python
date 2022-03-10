1c9f63a91f96742b90aff94f7252fb323833cfa4
jazSingh5 <44950413+jazSingh5@users.noreply.github.com>
2022-03-10 11:06:45 -0800

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -1,11 +1,12 @@
 import time
 
 
-def wrap_func(*args, **kwargs):
+def timer_func(func):
+
+    def wrap_func(*args, **kwargs):
         t1 = time.time()
         result = func(*args, **kwargs)
         t2 = time.time()
         print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
         return result
     return wrap_func
-  
