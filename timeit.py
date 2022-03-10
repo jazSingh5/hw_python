8444fc51f8ac1601e2bb1bf11945c4126dcac67e
jazSingh5 <44950413+jazSingh5@users.noreply.github.com>
2022-03-10 12:16:31 -0800

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -1,12 +1,19 @@
+from functools import wraps
 import time
 
-
-def calculate_time(func):
-
-    def wrap_func(*args, **kwargs):
-        t1 = time.time()
-        result = func(*args, **kwargs)
-        t2 = time.time()
-        print(f'Function {func.__name__!r} executed in {(t2-t1):.4f}s')
+def timing(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+        print ('func:%r args:[%r, %r] took: %2.4f sec' % \
+          (f.__name__, args, kw, te-ts))
         return result
-    return wrap_func
+    return wrap
+
+@timing
+def f(a):
+    for _ in range(a):
+        i = 0
+    return -1
