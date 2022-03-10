ce37f92e7b11e2882b2310d04d9a6b8476b62fa5
jazSingh5 <44950413+jazSingh5@users.noreply.github.com>
2022-03-10 10:43:51 -0800

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -5,9 +5,9 @@ from functools import wraps
 def calculate_time(f):
     @wraps(f)
     def wrap(*args, **kw):
-        ts = time()
+        ts = time.time()
         result = f(*args, **kw)
-        te = time()
+        te = time.time()
         print('Total time took: %2.4f sec' %
               (f.__name__, args, kw, te-ts))
         return result
