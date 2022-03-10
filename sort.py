26c48e24deffd7225df974332abe152eaa0f48cc
jazSingh5 <44950413+jazSingh5@users.noreply.github.com>
2022-03-09 23:04:29 -0800

    Update sort.py

--- sort.py
+++ sort.py
@@ -7,14 +7,12 @@ def sort_list(lst):
 
     while (i < n):
         d = 0
-        while (d <= n-1):
-            if (lst[i] > lst[d]):
-                lst[i], lst[d] = lst[d], lst[i]
+        while (d <= n-i-1):
+            if (lst[d+1] > lst[d]):
+                lst[d+1], lst[d] = lst[d+1], lst[d]
             d += 1
         i += 1
     return lst
 
 
-lst = [5, 4, 3, 9, 77, 5, 6, 7]
-sort_list(lst)
-print(lst)
+
