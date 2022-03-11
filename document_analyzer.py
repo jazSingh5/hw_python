0747daf7a9d2a4ea0c2bec1067700b980effa503
jazSingh5 <44950413+jazSingh5@users.noreply.github.com>
2022-03-10 21:00:11 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -1,13 +1,78 @@
-def sort_list(lst):
+import string
+d = dict()
+text = open("document.txt", encoding="utf8")
+for line in text:
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    line = line.strip()
+    words = line.split(" ")
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+x = {}
+for key in list(d.keys()):
+    x[key] = d[key]
+sort = sorted(x.items(), key=lambda x: (-x[1], x[0]))
+top5 = list(sort)[:5]
+print(" ")
+for i in top5:
+    import string
+d = dict()
+text = open("document.txt", encoding="utf8")
+for line in text:
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    line = line.strip()
+    words = line.split(" ")
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+x = {}
+for key in list(d.keys()):
+    x[key] = d[key]
+sort = sorted(x.items(), key=lambda x: (-x[1], x[0]))
+top5 = list(sort)[:5]
+print(" ")
+for i in top5:
 
-    n = len(lst)
-    i = 1
-
-    while (i < n):
-        d = 0
-        while (d <= n-1):
-            if (lst[i] > lst[d]):
-                lst[i], lst[d] = lst[d], lst[i]
-            d += 1
-        i += 1
-    return lst
+    import string
+d = dict()
+text = open("document.txt", encoding="utf8")
+for line in text:
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    line = line.strip()
+    words = line.split(" ")
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+x = {}
+for key in list(d.keys()):
+    x[key] = d[key]
+sort = sorted(x.items(), key=lambda x: (-x[1], x[0]))
+top5 = list(sort)[:5]
+print(" ")
+for i in top5:
+    print(f"import string")
+d = dict()
+text = open("document.txt", encoding="utf8")
+for line in text:
+    line = line.translate(line.maketrans("", "", string.punctuation))
+    line = line.strip()
+    words = line.split(" ")
+    for word in words:
+        if word in d:
+            d[word] = d[word] + 1
+        else:
+            d[word] = 1
+x = {}
+for key in list(d.keys()):
+    x[key] = d[key]
+sort = sorted(x.items(), key=lambda x: (-x[1], x[0]))
+top5 = list(sort)[:5]
+print(" ")
+for i in top5:
+    print(f"{i[0]}: {i[1]}")
