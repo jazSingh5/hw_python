660df25fc5104741c1ea37c43852fa99d8f9f2ef
jazSingh5 <44950413+jazSingh5@users.noreply.github.com>
2022-03-10 21:03:58 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -37,42 +37,4 @@ top5 = list(sort)[:5]
 print(" ")
 for i in top5:
 
-    import string
-d = dict()
-text = open("document.txt", encoding="utf8")
-for line in text:
-    line = line.translate(line.maketrans("", "", string.punctuation))
-    line = line.strip()
-    words = line.split(" ")
-    for word in words:
-        if word in d:
-            d[word] = d[word] + 1
-        else:
-            d[word] = 1
-x = {}
-for key in list(d.keys()):
-    x[key] = d[key]
-sort = sorted(x.items(), key=lambda x: (-x[1], x[0]))
-top5 = list(sort)[:5]
-print(" ")
-for i in top5:
-    print(f"import string")
-d = dict()
-text = open("document.txt", encoding="utf8")
-for line in text:
-    line = line.translate(line.maketrans("", "", string.punctuation))
-    line = line.strip()
-    words = line.split(" ")
-    for word in words:
-        if word in d:
-            d[word] = d[word] + 1
-        else:
-            d[word] = 1
-x = {}
-for key in list(d.keys()):
-    x[key] = d[key]
-sort = sorted(x.items(), key=lambda x: (-x[1], x[0]))
-top5 = list(sort)[:5]
-print(" ")
-for i in top5:
-    print(f"{i[0]}: {i[1]}")
+  
